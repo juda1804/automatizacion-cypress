a66200bf0d1e4a6abd21b09601215edbbf8b6033
Juan Cadavid <juancadavid@seven4n.com>
2020-06-08 11:19:27 -0500

    segundo caso de prueba, aun no funciona correctamente

--- cypress/integration/sample.js
+++ cypress/integration/sample.js
@@ -1,17 +1,30 @@
 describe('Test suit', function() {
+    const urlBase = 'http://demo.guru99.com/V4/index.php'
+    const emailid = '13@poli.edu.co'
+    const userid = 'mngr264622'
+    const password = 'UvegabU'
+    let customerId = ''
+
+    Cypress.on('uncaught:exception', (err, runnable) => {
+        // returning false here prevents Cypress from
+        // failing the test
+        return false
+    })
+
     it('Test Case 1',function() {
-        cy.visit('http://demo.guru99.com/V4/index.php')
-        cy.get('input[name="uid"]').type('mngr264622') 
-        cy.get('input[name="password"]').type('UvegabU') 
+        cy.visit(urlBase)
+        cy.get('input[name="uid"]').type(userid) 
+        cy.get('input[name="password"]').type(password) 
         cy.get('input[name="btnLogin"]').click()
-        cy.contains('mngr26462')
+        cy.contains(password)
     })
 
 
     it.only('Test Case 2',function() {
-        cy.visit('http://demo.guru99.com/V4/index.php')
-        cy.get('input[name="uid"]').type('mngr264622') 
-        cy.get('input[name="password"]').type('UvegabU') 
+        cy.clock()
+        cy.visit(urlBase)
+        cy.get('input[name="uid"]').type(userid) 
+        cy.get('input[name="password"]').type(password)  
         cy.get('input[name="btnLogin"]').click()
         
 
@@ -19,28 +32,41 @@ describe('Test suit', function() {
         cy.visit('http://demo.guru99.com/V4/manager/addcustomerpage.php')
         cy.get('input[name="name"]').type('Juan') 
         
+        const targetDate = Cypress.moment()
+        .subtract(30, 'year')
+        .add(1, 'month')
+        .add(1, 'day')
+        .format('YYYY-MM-DD') 
+
+        cy.get('input[name="dob"]')
+        .type(`${targetDate}`)
+
         cy.get('textarea[name="addr"]').type('Lopez')
         cy.get('input[name="city"]').type('Medellin ')  
         cy.get('input[name="state"]').type('Juan') 
-        cy.get('input[name="pinno"]').type('000') 
-
-        const targetDate = Cypress.moment()
-        .add(1, 'year')
-        .add(1, 'month')
-        .add(1, 'day')
-        .format('MM/DD/YYYY') 
-
-        cy.get('input[name="dob"]')
-        .clear()
-        .type(`${targetDate}{enter}`)
-
+        cy.get('input[name="pinno"]').type('264622') 
         cy.get('input[name="telephoneno"]').type('000000')
-        cy.get('input[name="emailid"]').type('juan_david@u.com') 
-        cy.get('input[name="password"]').type('asd123') 
-        cy.get('input[name="submit"]').click()
+        cy.get('input[name="emailid"]').type(emailid) 
+        cy.get('input[name="password"]').type('pass') 
+        cy.get('input[name="sub"]').click()   
+        cy.tick(500)
+        customerId = cy.get('table[name="customer"] > tbody > tr:nth-child(4) > td:nth-child(2)')
 
+        console.log("hola munod",customerId)
+        //cy.visit('http://demo.guru99.com/V4/manager/addAccount.php')
+
+        //cy.get('input[name="cusid"]').type(customerId)
         
 
-        
+    })
+
+    it('Test Case 3',function() {
+        cy.visit(urlBase)
+        cy.get('input[name="uid"]').type(userid) 
+        cy.get('input[name="password"]').type(password)  
+        cy.get('input[name="btnLogin"]').click()
+
+        cy.visit('http://demo.guru99.com/V4/manager/addAccount.php')
+        cy.get('textarea[name="cusid"]').type(customerId)
     })
 }) 
