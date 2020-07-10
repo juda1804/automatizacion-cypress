abceb4db27317bf12842bd928d4c0c32c6415927
Juan Cadavid <juancadavid@seven4n.com>
2020-07-10 18:00:39 -0500

    7.Cambiar a la rama conflicto y realizar commit.

--- cypress/integration/scenaries.js
+++ cypress/integration/scenaries.js
@@ -12,7 +12,7 @@ describe('Test suit', function() {
     var emailid = myNumeroAleatorio + "@poli.edu.co"
     var accountNo = "";
     var transaccionId = "";
-    const userid = ''
+    const userid = '1313'
     const password = 'UvegabU'
     const pinno = '264622'
     const deposit = 1000
